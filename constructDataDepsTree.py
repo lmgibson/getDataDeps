67393b844c983cbdd5d15367012841e892e752cc
lmgibson <landon.g42@gmail.com>
2021-01-15 21:16:35 -0800

    Removed unneccesary code

--- constructDataDepsTree.py
+++ constructDataDepsTree.py
@@ -1,13 +1,8 @@
 import json
 import pydot
-import pprint
 
 with open('./dataDeps.json') as json_file:
     data = json.load(json_file)
-
-# data = {'A': {'To': ['B', 'C'], 'From': None},
-#         'B': {'To': None, 'From': 'A'},
-#         'C': {'To': None, 'From': 'A'}}
 
 
 def createDepGraph(data):
@@ -39,18 +34,4 @@ def createDepGraph(data):
 
 
 graph = createDepGraph(data)
-graph.write_svg('test.svg')
-
-# dot_graph = pydot.Dot(graph_type='digraph')
-
-# sd_node = pydot.Node('A')
-# sd_node.set_shape('box3d')
-# dot_graph.add_node(sd_node)
-
-# qsd_node = pydot.Node('B')
-# qsd_node.set_shape('box3d')
-# dot_graph.add_node(qsd_node)
-
-# iedge = pydot.Edge(sd_node, qsd_node)
-# iedge.set_label('Tables')
-# dot_graph.add_edge(iedge)
+graph.write_svg('dataDepsGraph.svg')
