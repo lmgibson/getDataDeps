da92768a0e2d6f9a3a9956279f6d914f39e01cac
lmgibson <landon.g42@gmail.com>
2021-01-28 08:31:55 -0800

    I think I can use awk to select out the column where the dataset is referenced (stata commands). This is because in stata the spaces matter and the input is very structured. The challenge is managing all the ways data may be input.

--- getDataDeps.py
+++ getDataDeps.py
@@ -45,9 +45,9 @@ def extractDataDeps(allCodeFiles):
                 'cat ' + file + ' | grep -A 1 "readRDS*\|read_csv*" | grep -o \'".*"\' | sed \'s/"//g\' ').read()
         elif '.do' in file:
             save = os.popen(
-                'cat ' + file + ' | grep "save" | awk \'{print $2}\' ').read()
+                'cat ' + file + ' | grep "save\|sysuse" | awk \'{print $2}\' ').read()
             read = os.popen(
-                'cat ' + file + ' | grep -A 1 "infile\|use" | grep -o \'".*"\' | sed \'s/"//g\' ').read()
+                'cat ' + file + ' | grep -A 1 "infile\|use" | awk \'{print $2}\' ').read()
         else:
             save = os.popen(
                 'cat ' + file + ' | grep -A 1 "saveRDS*\|write[_.]csv*\|to_csv*" | grep -o \'".*"\' | sed \'s/"//g\' ').read()
