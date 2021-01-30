2d6723f36454754f6cad06c5a41b8b087cb34285
lmgibson <landon.g42@gmail.com>
2021-01-29 22:34:41 -0800

    Added initial support for SQL. It assumes SQL calls are made within an R or python file. The SQL call can be either one line or spread across multiple lines. What matter is that FROM and the database are called on the same line.

--- examples/normalProjectStructure/code/01_dataConstruct/01A_analyticFile.R
+++ examples/normalProjectStructure/code/01_dataConstruct/01A_analyticFile.R
@@ -1,11 +1,14 @@
 library(tidyverse)
 
-dt = tibble(
+dt <- tibble(
     "A" = c(rep(0, 1000)),
     "B" = c(rep(1, 1000))
-    )
+)
 
-saveRDS(dt,
-    "./data/analytic.rds")
+sqlCall <- "SELECT * FROM db.dba.hello FROM something.dba.hi"
+saveRDS(
+    dt,
+    "./data/analytic.rds"
+)
 saveRDS(dt, "./data/test.rds")
 saveRDS(dt, "./data/notUsedData.rds")
--- test_getDataDeps.py
+++ test_getDataDeps.py
@@ -23,16 +23,16 @@ def test_getListOfFiles_content():
 
 
 def test_extractDataDeps_structure():
-    example_file_structure = expected = ['./examples/normalProjectStructure/code/03_modeling/03A_train.py', './examples/normalProjectStructure/code/04_modelResults/04A_stataCode.do',
-                                         './examples/normalProjectStructure/code/01_dataConstruct/01A_analyticFile.R', './examples/normalProjectStructure/code/02_analyses/02B_featureEngineering.R', './examples/normalProjectStructure/code/02_analyses/02A_descriptives.R']
+    example_file_structure = ['./examples/normalProjectStructure/code/03_modeling/03A_train.py', './examples/normalProjectStructure/code/04_modelResults/04A_stataCode.do',
+                              './examples/normalProjectStructure/code/01_dataConstruct/01A_analyticFile.R', './examples/normalProjectStructure/code/02_analyses/02B_featureEngineering.R', './examples/normalProjectStructure/code/02_analyses/02A_descriptives.R']
     data, save, read = getDataDeps.extractDataDeps(example_file_structure)
 
     assert save is not None
-    assert len(save) == 6
+    assert len(save) == 7
 
     assert read is not None
     assert len(read) == 5
 
     assert data is not None
-    assert len(data) == 6
-    assert [len(data[x]) for x in data] == [2, 2, 2, 2, 2, 2]
+    assert len(data) == 8
+    assert [len(data[x]) for x in data] == [2, 2, 2, 2, 2, 2, 2, 2]
